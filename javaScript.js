add8077572d7ef613af635aaf55f6b3eb9caa430
gingerkeys <gkeys@basecampcodingacademy.org>
2019-01-30 16:29:23 -0600

    added the different characters on their karts.

--- javaScript.js
+++ javaScript.js
@@ -2,7 +2,34 @@
 
 const BIG_IMAGE_SRC = {
     Mario: "./images/Mario_Kart.png",
-    Luigi: "./images/Luigi_Kart.png"
+    Luigi: "./images/Luigi_Kart.png",
+    Peach: "./images/Peach_Kart.png",
+    Daisy: "./images/Daisy_Kart.png",
+    Rosalina: "./images/Rosalina_Kart.png",
+    Rmario: "./images/Rmario_Kart.png",
+    Catpeach: "./images/Catpeach_Kart.png",
+    Yoshi: "./images/Yoshi_Kart.png",
+    Toad: "./images/Toad_Kart.png",
+    Koopa: "./images/Koopa_Kart.png",
+    ShyGuy: "./images/Shyguy_Kart.png",
+    Lakitu: "./images/Lakitu_Kart.png",
+    Toadette: "./images/Toadette_Kart.png",
+    KingBoo: "./images/KingBoo_Kart.png",
+    Babymario: "./images/Babymario_Kart.png",
+    Babypeach: "./images/Babypeach_Kart.png",
+    Babydaisy: "./images/Babydaisy_Kart.png",
+    Babyrosalina: "./images/Babyrosalina_Kart.png",
+    Metalmario: "./images/Metalmario_Kart.png",
+    Pinkpeach: "./images/Pinkgoldpeach_Kart.png",
+    Wario: "./images/Wario_Kart.png",
+    Waluigi: "./images/Waluigi_Kart.png",
+    Donkeykong: "./images/Donkeykong_Kart.png",
+    Bowser: "./images/Bowser_Kart.png",
+    Drybones: "./images/Drybones_Kart.png",
+    Bowserjr: "./images/Bowserjr_Kart.png",
+    Drybowser: "./images/Drybowser_Kart.png",
+    Lemmy: "./images/Lemmy_Kart.png",
+    Larry: "./images/Larry_Kart.png"
 };
 
 function listenForChange() {
