608d25bbe33bf804435d8bb0b6be10d1cb88c979
gingerkeys <gkeys@basecampcodingacademy.org>
2019-02-01 16:34:43 -0600

    added some different pictures, the name above the character, and a transform animation.

--- javaScript.js
+++ javaScript.js
@@ -6,28 +6,28 @@ const BIG_IMAGE_SRC = {
     Peach: "./images/Peach_Kart.png",
     Daisy: "./images/Daisy_Kart.png",
     Rosalina: "./images/Rosalina_Kart.png",
-    Rmario: "./images/Rmario_Kart.png",
-    Catpeach: "./images/Catpeach_Kart.png",
+    "Tanooki Mario": "./images/Rmario_Kart.png",
+    "Cat Peach": "./images/Catpeach_Kart.png",
     Yoshi: "./images/Yoshi_Kart.png",
     Toad: "./images/Toad_Kart.png",
     Koopa: "./images/Koopa_Kart.png",
     ShyGuy: "./images/Shyguy_Kart.png",
     Lakitu: "./images/Lakitu_Kart.png",
     Toadette: "./images/Toadette_Kart.png",
-    KingBoo: "./images/KingBoo_Kart.png",
-    Babymario: "./images/Babymario_Kart.png",
-    Babypeach: "./images/Babypeach_Kart.png",
-    Babydaisy: "./images/Babydaisy_Kart.png",
-    Babyrosalina: "./images/Babyrosalina_Kart.png",
-    Metalmario: "./images/Metalmario_Kart.png",
-    Pinkpeach: "./images/Pinkgoldpeach_Kart.png",
+    "King Boo": "./images/KingBoo_Kart.png",
+    "Baby Mario": "./images/Babymario_Kart.png",
+    "Baby Peach": "./images/Babypeach_Kart.png",
+    "Baby Daisy": "./images/Babydaisy_Kart.png",
+    "Baby Rosalina": "./images/Babyrosalina_Kart.png",
+    "Metal Mario": "./images/Metalmario_Kart.png",
+    "Pink Peach": "./images/Pinkgoldpeach_Kart.png",
     Wario: "./images/Wario_Kart.png",
     Waluigi: "./images/Waluigi_Kart.png",
-    Donkeykong: "./images/Donkeykong_Kart.png",
+    "Donkey Kong": "./images/Donkeykong_Kart.png",
     Bowser: "./images/Bowser_Kart.png",
-    Drybones: "./images/Drybones_Kart.png",
-    Bowserjr: "./images/Bowserjr_Kart.png",
-    Drybowser: "./images/Drybowser_Kart.png",
+    "Dry Bones": "./images/Drybones_Kart.png",
+    "Bowser Jr": "./images/Bowserjr_Kart.png",
+    "Dry Bowser": "./images/Drybowser_Kart.png",
     Lemmy: "./images/Lemmy_Kart.png",
     Larry: "./images/Larry_Kart.png",
     Wendy: "./images/Wendy_Kart.png",
@@ -35,22 +35,23 @@ const BIG_IMAGE_SRC = {
     Iggy: "./images/Iggy_Kart.png",
     Roy: "./images/Roy_Kart.png",
     Morton: "./images/Morton_Kart.png",
-    Inklinggirl: "./images/Inklinggirl_Kart.png",
-    Inklingboy: "./images/Inklingboy_Kart.png",
+    "Inkling Girl": "./images/Inklinggirl_Kart.png",
+    "Inkling Boy": "./images/Inklingboy_Kart.png",
     Link: "./images/Link_Kart.png"
 };
 
 function listenForChange() {
     const characters = document.querySelectorAll(".character");
     const chosenCharacter = document.getElementById("chosenCharacter");
+    const name = document.getElementById("chosenCharacterName");
 
     for (const characterElement of characters) {
         const characterName = characterElement.dataset.character;
 
-        characterElement.addEventListener(
-            "mouseover",
-            () => (chosenCharacter.src = BIG_IMAGE_SRC[characterName])
-        );
+        characterElement.addEventListener("mouseover", () => {
+            chosenCharacter.src = BIG_IMAGE_SRC[characterName];
+            name.innerText = characterName;
+        });
     }
 }
 
